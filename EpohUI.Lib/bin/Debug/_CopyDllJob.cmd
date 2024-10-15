dfd78a04a681ed860a0e026c1b75b868ec5fe724
sxydh <sxydhgg@gmail.com>
2024-10-16 01:23:36 +0800

    [UPDATE]

--- EpohUI.Lib/bin/Debug/_CopyDllJob.cmd
+++ EpohUI.Lib/bin/Debug/_CopyDllJob.cmd
@@ -5,6 +5,10 @@ setlocal enabledelayedexpansion
 set "watchFolder=%cd%"
 set "targetFolder=%cd%\..\..\..\EpohUI.Core\bin\Debug\DLLs"
 
+pushd "%targetFolder%"
+set "targetFolder=%cd%"
+popd
+
 dir /b "%watchFolder%\*.dll" > _FileList.txt 2> nul
 
 :loop
@@ -14,12 +18,10 @@ dir /b "%watchFolder%\*.dll" > _NewFileList.txt 2> nul
 
 fc /b _FileList.txt _NewFileList.txt > nul
 if errorlevel 1 (
-    echo "文件夹发生变化，清空 target 文件夹并复制 DLL 文件。"
-
     echo "正在清空 %targetFolder%"
     del "%targetFolder%\*.dll" 2> nul
 
-    echo "正在复制 DLL 文件到 %targetFolder%"
+    echo "正在复制 %watchFolder%"
     copy "%watchFolder%\*.dll" "%targetFolder%\" 2> nul
 
     copy /y _NewFileList.txt _FileList.txt
