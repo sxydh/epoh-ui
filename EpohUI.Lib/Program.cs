cc6483c3c2ed94bc694d17edd667dabf3442ebb2
sxydh <sxydhgg@gmail.com>
2024-10-15 10:53:26 +0800

    [UPDATE]

---
+++ EpohUI.Lib/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace EpohUI.Lib
+{
+    public class Program
+    {
+
+        public static void Kill()
+        {
+            try
+            {
+                Process.GetCurrentProcess().Kill();
+            }
+            catch { }
+        }
+
+        public static string GetMethodIdMap()
+        {
+            return $"lib/program-kill={typeof(Program).FullName}#Kill";
+        }
+
+    }
+}
