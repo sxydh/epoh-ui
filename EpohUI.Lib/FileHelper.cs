f2d0366065dc6204ea12a4db6814152882398235
sxydh <sxydhgg@gmail.com>
2024-10-16 10:25:58 +0800

    [UPDATE]

--- EpohUI.Lib/FileHelper.cs
+++ EpohUI.Lib/FileHelper.cs
@@ -1,10 +1,11 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace EpohUI.Lib
 {
     public class FileHelper
     {
-
         public static string Read(string reqBody)
         {
             return File.ReadAllText(reqBody);
@@ -15,6 +16,14 @@ namespace EpohUI.Lib
             return new FileStream(reqBody, FileMode.Open, FileAccess.Read, FileShare.Read, bufferSize: 4096);
         }
 
+        public static void Write(string reqBody)
+        {
+            var reqJon = JObject.Parse(reqBody);
+            var file = reqJon["file"] ?? throw new ArgumentException("Args file cannot be null");
+            var text = reqJon["text"] ?? throw new ArgumentException("Args text cannot be null");
+            File.WriteAllText(file.ToString(), text.ToString());
+        }
+
         public static string GetMethodIdMap()
         {
             var ret = "";
@@ -22,6 +31,5 @@ namespace EpohUI.Lib
             ret += $"\r\nlib/file-read_stream={typeof(FileHelper).FullName}#ReadStream";
             return ret;
         }
-
     }
 }
