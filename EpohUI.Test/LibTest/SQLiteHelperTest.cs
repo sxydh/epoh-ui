529dc6a348d7fb6ddaba548f9fa3254dc0410754
sxydh <sxydhgg@gmail.com>
2024-10-16 12:40:05 +0800

    [UPDATE]

--- EpohUI.Test/LibTest/SQLiteHelperTest.cs
+++ EpohUI.Test/LibTest/SQLiteHelperTest.cs
@@ -1,27 +1,24 @@
 ﻿using EpohUI.Lib;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
-using System.Collections.Generic;
 
 namespace EpohUI.Test.LibTest
 {
-
     [TestClass]
     public class SQLiteHelperTest
     {
-
         [TestMethod]
         public void SelectTest()
         {
-            var req = new Dictionary<string, object>
+            var req = new SQLiteReq
             {
-                ["sql"] = "select #p1 as i, #p2 as s, #p3 as n",
-                ["args"] = new object[] { 1, "1", null },
-                ["file"] = $"{typeof(SQLiteHelperTest).FullName}.sqlite"
+                Sql = "select #p1 as i, #p2 as s, #p3 as n",
+                Args = new object[] { 1, "1", null },
+                File = $"{typeof(SQLiteHelperTest).FullName}.sqlite"
             };
-            var ret = SQLiteHelper.Execute(JsonConvert.SerializeObject(req));
+            var reqStr = JsonConvert.SerializeObject(req);
+            var ret = SQLiteHelper.Execute(reqStr);
             Assert.IsNotNull(ret);
         }
-
     }
 }
