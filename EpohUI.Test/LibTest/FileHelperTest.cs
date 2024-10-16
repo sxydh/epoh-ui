e5a1a9e7f650d680a50765c529d7bebc7bc3bd50
sxydh <sxydhgg@gmail.com>
2024-10-16 10:44:40 +0800

    [UPDATE]

---
+++ EpohUI.Test/LibTest/FileHelperTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using EpohUI.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace EpohUI.Test.LibTest
+{
+    [TestClass]
+    public class FileHelperTest
+    {
+        [TestMethod]
+        public void TestWrite()
+        {
+            var dict = new Dictionary<string, string>
+            {
+                ["file"] = Path.Combine(Directory.GetCurrentDirectory(), $"{typeof(FileHelperTest).FullName}.TestWrite"),
+                ["text"] = DateTime.Now.ToString(CultureInfo.InvariantCulture)
+            };
+            FileHelper.Write(JsonConvert.SerializeObject(dict));
+            Assert.IsTrue(File.Exists(dict["file"]));
+            File.Delete(dict["file"]);
+            Assert.IsTrue(!File.Exists(dict["file"]));
+        }
+    }
+}
